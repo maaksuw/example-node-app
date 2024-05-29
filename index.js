fbdbb0a718e37a04817a8464793181483982b10b
Akseli Vierimaa <akseli.vierimaa@helsinki.fi>
2024-05-29 18:18:18 +0300

    Test watchtower with code change.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>I hope you have a good day today!</h1>')
 })
 
 const PORT = 8080
